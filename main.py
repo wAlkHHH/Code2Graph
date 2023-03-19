c397ee37d7dfd8ea4fe27c6f6a86d52e7dd4773f
hbr <huangbr0315@outlook.com>
2023-03-19 22:57:42 +0800

    add pickle file to load existed pickle file and save the input graph

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 #! python3
 import matplotlib.pyplot as plt
 import networkx as nx
+import openpyxl as pxl
+import os
+import pickle
+import datetime
 
-left, right, bottom, top, layer_sizes = .1, .9, .1, .9, [4, 7, 7, 2]
-# 网络离上下左右的距离
-# layter_sizes可以自己调整
 
-if __name__ ==  "__main__":
-    G = nx.DiGraph()
-    pos = {}
-    cnt = 0;
+def inputMode():
     while True :
-        print("输入起始点，以空格分隔，第一个参数为起点，或者输入end结束,输入quit直接退出")
+        print("输入起始点，以空格分隔，第一个参数为起点，或者输入end结束")
         str = input()
         nodes = str.split(" ")
         n = len(nodes)
@@ -19,18 +17,63 @@ if __name__ ==  "__main__":
             continue
         elif n == 1 :
             if str.lower().startswith("end") :
-                print("开始构建图......")
                 break
-            elif str.lower().startswith("quit") :
-                print("退出...")
-                exit(0)
             else :
                 continue
         G.add_node(nodes[0])
-        pos[nodes[0]] = (cnt, cnt*cnt/2)
+        graph.setdefault(nodes[0], set())
         for i in range(1, len(nodes)) :
             G.add_edge(nodes[0], nodes[i])
+            graph[nodes[0]].add(nodes[i])
 
+def fileMode():
+    while True :
+        print("输入pickle文件的路径或者输入end结束")
+        str = input()
+        if str.lower().startswith("end") :
+            break
+
+        str = os.path.abspath(str)
+        if not os.path.exists(str) :
+            print("文件不存在")
+            continue
+        with open(str, "rb") as f :
+            oldGraph = pickle.load(f)
+        if not isinstance(oldGraph, dict) :
+            print("文件类型错误")
+            continue
+        
+        for start, ends in oldGraph :
+            G.add_node(start)
+            graph.setdefault(start, set())
+            for node in ends :
+                G.add_edge(start, node)
+                graph[start].add(node)
+                
+
+        
+
+
+        
+            
+graph = {}
+G = nx.DiGraph()
+
+if __name__ ==  "__main__":
+    pos = {}
+    cnt = 0;
+    while True :
+        print("选择不同的模式，或者输入end结束,输入quit直接退出")
+        str = input()
+        if str.lower().startswith("end") :
+            break
+        elif str.lower().startswith("quit") :
+            print("退出...")
+            exit(0)
+        elif str.lower().startswith("i") :
+            inputMode()
+        elif str.lower().startswith("e") :
+            fileMode()
     print("开始构建......")
     options = {
         "font_size": 36,
@@ -54,3 +97,6 @@ if __name__ ==  "__main__":
     fig.tight_layout()
     plt.show()
     print("构建完成......")
+    saveFile = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    with open(saveFile, "wb") as f :
+        pickle.dump(graph, f)
