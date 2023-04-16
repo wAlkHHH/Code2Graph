7ecfd0e1f613594c65500f3e097ba8fb036c5e69
hbr <huangbr0315@outlook.com>
2023-04-16 16:31:16 +0800

    add the option that scans all the .c file from the inputted path and generated graph

--- ReCode.py
+++ ReCode.py
@@ -2,18 +2,44 @@
 # ReCode.py
 
 import re
+from pyvis import network as vis_net
+import networkx as nx
 # pattern = re.compile(r'''[a-zA-Z][\s\w]+\s+ # 必须从字母开始
 #     ([\w]+)   # 函数名
 #     \([\s\w]*\) # 函数入参
 #     \s*\{([\s\S]*?)\}''') # 函数体，非贪婪匹配
 # 正则表达式可视化 https://c.runoob.com/front-end/7625/#!flags=&re=
 
+def ProcCodeText2Graph(codeText, codeGraph) :
+    pattern = re.compile(r'''[a-zA-Z][\s\w]+\s+([\w]+)\([\s\w*]*\)\s*\{([\s\S]*?)\}''')
+    pattern_for_func = re.compile(r'''([\w]+)\([\W\w]*?\)''')
+    # with open('TestText.txt', 'r') as f:
+    #     codeText = f.read()
 
-pattern = re.compile(r'''[a-zA-Z][\s\w]+\s+([\w]+)\([\s\w]*\)\s*\{([\s\S]*?)\}''')
-with open('TestText.txt', 'r') as f:
-    str = f.read()
+    # print(pattern.search(str).group(0))
+    # codeGraph = dict()
+    for g in pattern.findall(codeText) :
+        print(g)
+        codeGraph.setdefault(g[0], set())
+        for func in pattern_for_func.findall(g[1]) :
+            print(func)
+            codeGraph[g[0]].add(func)
+    return codeGraph
 
-print(pattern.search(str).group(0))
+if __name__ ==  "__main__":
 
-for g in pattern.findall(str) :
-    print(g)
+    G = nx.DiGraph()
+    codeGraph = dict()
+    for src, dsts in codeGraph.items() :
+        G.add_node(src)
+        print("src: %s"%src)
+        for dst in dsts :
+            G.add_edge(src, dst)
+            print("dst: %s"%dst)
+    nt = vis_net.Network("500px", "1500px", directed=True)
+    nt = vis_net.Network("500px", "1500px", directed=True)
+    nt.from_nx(G)
+    nt.show_buttons(filter_=['physics'])
+    # TO DO -- check if the file is exited
+    nt.show("nx.html", notebook=False)
+    print("构建完成......")
--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import pickle
 import datetime
 from pyvis import network as vis_net
 import networkx as nx
+import ReCode
 
 tab_colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'white']
-
+graph = {}
+G = nx.DiGraph()
 
 def inputMode():
     while True :
@@ -53,16 +55,42 @@ def fileMode():
             for node in ends :
                 G.add_edge(start, node)
                 graph[start].add(node)
-                
 
-        
+def transferDiyG2NxG():
+    for start, ends in graph.items() :
+        G.add_node(start)
+        for node in ends :
+            G.add_edge(start, node)
+            graph[start].add(node)
+
+def walkDirAndProcCode(absPath):
+    for folderName, subfolders, filenames in os.walk(absPath) :
+            for filename in filenames :
+                if not filename.lower().endswith(".c") :
+                    continue
+                with open(os.path.join(folderName, filename), "r") as f :
+                    codeText = f.read()
+                    ReCode.ProcCodeText2Graph(codeText, graph)
+            for subfolder in subfolders :
+                walkDirAndProcCode(os.path.join(folderName, subfolder))
 
 
-        
-            
-graph = {}
-G = nx.DiGraph()
-
+def dirMode():
+    while True :
+        print("输入代码目录路径或者输入end结束")
+        str = input()
+        if str.lower().startswith("end") :
+            break
+        # str = os.path.abspath(str)
+        if not os.path.exists(str) :
+            print("文件不存在")
+            continue
+        if str.endswith(".c") :
+            print("路径非目录")
+            continue
+        walkDirAndProcCode(str)
+    transferDiyG2NxG()
+    
 if __name__ ==  "__main__":
     pos = {}
     cnt = 0;
@@ -78,22 +106,24 @@ if __name__ ==  "__main__":
             inputMode()
         elif str.lower().startswith("e") :
             fileMode()
+        elif str.lower().startswith("dir") :
+            dirMode()
     print("开始构建......")
     colors_of_node = nx.coloring.greedy_color(G)
     print(colors_of_node)
-    print(list(G.nodes(data='color', default=None)))
     for node in list(G.nodes) :
         # G.nodes[node]['color'] = colors_of_node[node]
         G.nodes[node]['color'] = tab_colors[colors_of_node[node]%len(tab_colors)]
-        print(G.nodes[node]['color'])
-    print(list(G.nodes(data='color', default=None)))
-    nt = vis_net.Network("500px", "1500px", directed=True)
+        # print(G.nodes[node]['color'])
+    # print(list(G.nodes(data='color', default=None)))
+    nt = vis_net.Network("500px", "1000px", directed=True)
     nt.from_nx(G)
-    nt.show_buttons(filter_=['physics'])
+    nt.show_buttons(filter_=['physics', 'nodes', 'selection', 'layout'])
+
     # TO DO -- check if the file is exited
     nt.show("nx.html", notebook=False)
     print("构建完成......")
-    exit(0)
-    saveFile = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    with open(saveFile, "wb") as f :
-        pickle.dump(graph, f)
+    # TO DO -- save the files in other ways including the graph file
+    # saveFile = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    # with open(saveFile, "wb") as f :
+    #     pickle.dump(graph, f)
